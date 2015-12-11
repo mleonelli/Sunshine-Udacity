5ab224921502c34de2e268574467a1b10c20927b
Mauro Leonelli <mleonelli@gmail.com>
2015-12-11 10:37:12 +0100

    Weather data updated from API

---
+++ app/src/main/java/com/example/android/sunshine/app/DetailsActivity.java
@@ -0,0 +1,18 @@
+package com.example.android.sunshine.app;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class DetailsActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailsFragment())
+                    .commit();
+        }
+    }
+}
