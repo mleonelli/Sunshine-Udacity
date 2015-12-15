920dffc2bdd6b9b4019319ba840793cb02a0c3ee
Mauro Leonelli <mleonelli@gmail.com>
2015-12-15 18:17:41 +0100

    Added Settings stub

--- app/src/main/java/com/example/android/sunshine/app/DetailsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailsActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.MenuItem;
 
 public class DetailsActivity extends ActionBarActivity {
 
@@ -9,10 +11,28 @@ public class DetailsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new DetailsFragment())
                     .commit();
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent detailIntent = new Intent(this, SettingsActivity.class);
+            startActivity(detailIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
